ca06e3bd8d0cda2f501feb18a20a252d1ecec922
bcaure@gmail.com <bcaure@gmail.com@27e1ca8a-f869-09fd-b477-0e5c9aa286a7>
2013-08-20 11:02:07 +0000

    Correction persistence + divers améliorations

--- passwordPersistence/src/com/mdp/persistence/entity/SiteUser.java
+++ passwordPersistence/src/com/mdp/persistence/entity/SiteUser.java
@@ -5,65 +5,60 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
 
-
 /**
  * The persistent class for the user database table.
- * 
+ *
  */
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class SiteUser implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-	@Id
-	private String login;
-
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String login;
     @Temporal( TemporalType.DATE)
-	@Column(name="date_quota")
-	private Date dateQuota;
-
-	private String password;
-
-	@Column(name="used_quota")
-	private int usedQuota;
-    
-    @OneToMany(mappedBy="user")
+    @Column(name = "date_quota")
+    private Date dateQuota;
+    private String password;
+    @Column(name = "used_quota")
+    private int usedQuota;
+    @OneToMany(mappedBy = "user")
     private List<Compte> comptes;
 
     public SiteUser() {
     }
 
-	public String getLogin() {
-		return this.login;
-	}
+    public String getLogin() {
+        return this.login;
+    }
 
-	public void setLogin(String login) {
-		this.login = login;
-	}
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
-	public Date getDateQuota() {
-		return this.dateQuota;
-	}
+    public Date getDateQuota() {
+        return this.dateQuota;
+    }
 
-	public void setDateQuota(Date dateQuota) {
-		this.dateQuota = dateQuota;
-	}
+    public void setDateQuota(Date dateQuota) {
+        this.dateQuota = dateQuota;
+    }
 
-	public String getPassword() {
-		return this.password;
-	}
+    public String getPassword() {
+        return this.password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public int getUsedQuota() {
-		return this.usedQuota;
-	}
+    public int getUsedQuota() {
+        return this.usedQuota;
+    }
 
-	public void setUsedQuota(int usedQuota) {
-		this.usedQuota = usedQuota;
-	}
+    public void setUsedQuota(int usedQuota) {
+        this.usedQuota = usedQuota;
+    }
 
     public List<Compte> getComptes() {
         return comptes;
@@ -72,5 +67,4 @@ public class SiteUser implements Serializable {
     public void setComptes(List<Compte> comptes) {
         this.comptes = comptes;
     }
-
 }
